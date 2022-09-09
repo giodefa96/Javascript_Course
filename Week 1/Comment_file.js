91752f6088f77fa17a5e96766b62d838669abc2c
giodefa96 <giodefa96@gmail.com>
2022-09-09 20:40:46 +0200

    first week some ex.

--- Week 1/Comment_file.js
+++ Week 1/Comment_file.js
@@ -4,4 +4,7 @@ console.log('Hello world')
 ciao
 */
 
+console.log("%cHello, World", "color: blue; font-size: 40px")
 
+
+console.log("Hello " + "there, " + "World")
---
+++ Week 1/Variables.js
@@ -0,0 +1,21 @@
+var petDog = "Rex"
+var petCat = "Pepper"
+
+console.log(petDog)
+console.log(petCat)
+
+console.log("My pet dog's name is: " + petDog)
+
+console.log("My pet cat's name is: " , petCat)
+
+var catSound = 'purr'
+
+var dogSound = "woof"
+
+console.log(petDog + " Says " + dogSound)
+
+console.log(petCat + " Says " + catSound)
+
+var catSound = 'meow'
+
+console.log(petCat + " Says " + catSound)
